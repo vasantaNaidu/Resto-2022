5cfc6a0d1679db7cbe655e9004dddf6dabf7bed3
vasantaNaidu <ash123naidu@gmail.com>
2022-02-25 23:06:48 +0530

    11.06 ka server changes

---
+++ Server/app.js
@@ -0,0 +1,15 @@
+require('./config/db');
+
+const server = require('express')();
+const port = 3000;
+
+const UserRouter = require('./api/User');
+
+const bodyParser = require('express').json;
+server.use(bodyParser());
+
+server.use('/user', UserRouter)
+
+server.listen(port,() => {
+    console.log('Server Running on port');  
+})
