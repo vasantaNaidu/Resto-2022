3fdc00d36d65b48b648a38b1457161f01ff4c5b9
vasantaNaidu <ash123naidu@gmail.com>
2022-02-26 22:50:52 +0530

    Commit before heroku push

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,10 @@
 require('./config/db');
-
+	
 const server = require('express')();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const cors = require('cors');
+server.use(cors);
 
 const UserRouter = require('./api/User');
 
